a30d7d29b721f510b04d8b0a57aa02b1de5a53e8
AEGiner <andresesquivel2011@gmail.com>
2025-10-17 23:07:12 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomElement(array){
+ return array[Math.floor(Math.random() * array.length)]
+  
+}
+
 window.onload = function () {
   //write your code here
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
@@ -12,11 +17,7 @@ window.onload = function () {
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let randomwho = Math.floor(Math.random() * who.length)
-  let randomaction = Math.floor(Math.random() * action.length)
-  let randomwhat = Math.floor(Math.random() * what.length)
-  let randomwhen = Math.floor(Math.random() * when.length)
 
-  let randomExcuse = `${who[randomwho]} ${action[randomaction]} ${what[randomwhat]} ${when[randomwhen]}`
-  this.document.getElementById('excuse').innerHTML = randomExcuse
+  let randomExcuse = `${randomElement(who)} ${randomElement(action)} ${randomElement(what)} ${randomElement(when)}`
+  document.getElementById('excuse').innerHTML = randomExcuse
 };
